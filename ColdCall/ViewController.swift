1c32ce57b5023eceb380623b516c501b0495e6e9
Kaan <kaankabalak@gmail.com>
2017-07-12 15:14:08 -0700

    initial commit

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -10,14 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    let nameBank = ["Noah",
+                    "Emma",
+                    "William",
+                    "Olivia",
+                    "Ava",
+                    "Mason",
+                    "James",
+                    "Laura",
+                    "Benjamin",
+                    "Mia",
+                    "Jay",
+                    "Courtney",
+                    "Chris",
+                    "Emily"
+                    ]
+    
+    var randi = 0
+    var randnum = 0
+    
+    @IBAction func callButtonPressed(_ sender: UIButton) {
+        randi = Int(arc4random_uniform(UInt32(nameBank.count)))
+        randnum = Int(arc4random_uniform(UInt32(5)) + 1)
+        updateUI()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameLabel.text = "Ready?"
+        numberLabel.text = ""
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateUI() {
+        nameLabel.text = nameBank[randi]
+        numberLabel.text = String(randnum)
+        if (randnum <= 2) {
+            numberLabel.textColor = UIColor.red
+        }
+        else if (randnum <= 4) {
+            numberLabel.textColor = UIColor.orange
+        }
+        else {
+            numberLabel.textColor = UIColor.green
+        }
     }
 
 
